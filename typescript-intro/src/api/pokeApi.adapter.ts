266f731ffa7dc72b4f49a2d80bfae2d355ec27c3
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-12 19:47:28 -0300

    feat: principio de sustitución

--- typescript-intro/src/api/pokeApi.adapter.ts
+++ typescript-intro/src/api/pokeApi.adapter.ts
@@ -1,11 +1,25 @@
 import axios from "axios";
 
-export class PokeApiAdapter {
-    //Hacemos una clase privada y unicamente de lectura de axios: axios, que es un cliente HTTP basado en promesas para el navegador y node.js.
-    private readonly axios = axios;
+export interface HttpAdapter {
+  get<T>(url: string): Promise<T>;
+}
 
+export class PokeApiFetchAdapter implements HttpAdapter {
+  async get<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+    const data: T = await response.json();
+    console.log("con fetch tradicional");
+    return data;
+  }
+}
+//Se crea una clase llamada PokeApiAdapter
+export class PokeApiAdapter implements HttpAdapter{
+  //Hacemos una clase privada y unicamente de lectura de axios: axios, que es un cliente HTTP basado en promesas para el navegador y node.js.
+  private readonly axios = axios;
+  //Se crea un método llamado get, el cual recibe un parámetro de tipo string llamado url y retorna una promesa de tipo T, que es un tipo genérico.
   async get<T>(url: string): Promise<T> {
     //petición get
+    console.log("con axios");
     const { data } = await this.axios.get<T>(url);
     return data;
   }
--- typescript-intro/src/bases/04-injection.ts
+++ typescript-intro/src/bases/04-injection.ts
@@ -1,15 +1,15 @@
 import { Move, PokeapiResponse } from "../interface/pokeapi-response.interface";
-import { PokeApiAdapter } from "../api/pokeApi.adapter";
+import { HttpAdapter, PokeApiAdapter, PokeApiFetchAdapter } from "../api/pokeApi.adapter";
 
 export class Pokemon {
   get imageUrl(): string {
     return `https://pokemon.com/${this.id}.jpg`;
   } 
-
+  //se crea un constructor con los atributos id, name y http, el cual es de tipo PokeApiAdapter, que es una clase que se importa de la carpeta api.
   constructor(
     public readonly id: number,
     public name: string,
-    private readonly http: PokeApiAdapter // Todo: inyectar dependencias
+    private readonly http: HttpAdapter // Todo: inyectar dependencias
   ) {}
 
   scream() {
@@ -21,19 +21,19 @@ export class Pokemon {
   }
 
   async getMoves(): Promise<Move[]> {
-    // const { data } = await axios.get<PokeapiResponse>(
-    //   "https://pokeapi.co/api/v2/pokemon/4"
-    // );
-
+    //se crea una constante llamada data, la cual es de tipo PokeapiResponse y se le asigna el valor de la petición get que se hace a la url de la api de pokemon.
     const data = await this.http.get<PokeapiResponse>("https://pokeapi.co/api/v2/pokemon/4");
-    console.log(data.moves[0].move.name);
+    console.log(data.moves);
 
     return data.moves;
   }
 }
-
+//
 const pokeAPI = new PokeApiAdapter();
+const pokeApiFetch = new PokeApiFetchAdapter()
 
-export const charmander = new Pokemon(4, "Charmander", pokeAPI);
+export const charmander = new Pokemon(4, "charmander", pokeAPI);
+//const pikachu = new Pokemon(25, "pikachu", pokeApiFetch);
 
 charmander.getMoves();
+
