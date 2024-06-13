f4f628d0525bf0a8a6a0be83a46f1218252e8d87
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-13 17:38:08 -0300

    feat: decorators

---
+++ typescript-intro/src/bases/05-decorators.ts
@@ -0,0 +1,36 @@
+class NewPokemon {
+  constructor(public readonly id: number, public name: string) {}
+
+  scream() {
+    console.log(`ÑAOÑAO !!`);
+  }
+
+  speak() {
+    console.log(`soy un puto rebelde`);
+  }
+
+}
+
+const MyDecorator = () => {
+  return (target: Function) => {
+  return NewPokemon
+  };
+};
+
+@MyDecorator()  
+export class Pokemon {
+  constructor(public readonly id: number, public name: string) {}
+
+  scream() {
+    console.log(`${this.name.toUpperCase()} !!`);
+  }
+
+  speak() {
+    console.log(`${this.name}, ${this.name} `);
+  }
+}
+
+export const charmander = new Pokemon(4, "charmander");
+
+charmander.scream();
+charmander.speak();
