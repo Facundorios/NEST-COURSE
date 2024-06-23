2ce2f87f9d5a019fc5e2170e31ba323463d2ef80
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-23 01:43:20 -0300

    feat: added updated pokemon

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -38,14 +38,11 @@ export class PokemonService {
   }
 
   async findAll() {
-    
     let pokemons = this.pokemonModel.find();
-
     return pokemons;
   }
 
   async findOne(term: string) {
-
     let pokemon: Pokemon;
 
     //Busqueda por numero de pokemon
@@ -55,25 +52,52 @@ export class PokemonService {
       pokemon = await this.pokemonModel.findOne({ no: term });
     }
     //Busqueda por MongoID
-    //Segunda condición, se pregunta mediante el metodo "isValidObject" al term si es un id de Mongodb. De ser así, se utiliza el pokemonModel el metodo findById y se le pasa el term en donde retornará el pokemon con el MongoID perteneciente. 
+    //Segunda condición, se pregunta mediante el metodo "isValidObject" al term si es un id de Mongodb. De ser así, se utiliza el pokemonModel el metodo findById y se le pasa el term en donde retornará el pokemon con el MongoID perteneciente.
 
     if (isValidObjectId(term)) {
       pokemon = await this.pokemonModel.findById(term);
     }
     //busqueda por nombre
     //Tercera condición: Preguntamos Si term noes un numero y si además no es un ObjectID de mongo. De ser el caso se busca con el metodo de findOne por la propiedad de name. Para el manejo de minusculas y mayusculas, al term le añadimos el toLowerCase() así lo que pase, sin impoortar las min y mayus busque en minusculas en la db-
-    if ( isNaN(+term) && !isValidObjectId(term) ) {
-      pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() })
+    if (isNaN(+term) && !isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findOne({
+        name: term.toLowerCase().trim(),
+      });
     }
 
     if (!pokemon)
-      throw new NotFoundException(`Pokemon with parameter: "${term}" doesn't exist`);
+      throw new NotFoundException(
+        `Pokemon with parameter: "${term}" doesn't exist`,
+      );
 
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    //Para esto, debemos de filtrar el termino de busqueda, para saber el se buscapor alguna de la propiedades (name o no), o por su ObjectID, entonces para eso podemos llamar a la funcion previamente hecha para consultar un Pokemon según su término de bsuqueda.
+    try {
+      const pokemon = await this.findOne(term);
+      //Filtrado de mayusculas
+      if (updatePokemonDto.name) {
+        updatePokemonDto.name.toLowerCase();
+      }
+      //Actualiación
+      await pokemon.updateOne(updatePokemonDto, { new: true });
+
+      return pokemon;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(
+          `The Pokemon already exist in the Database: ${JSON.stringify(error.keyValue)}`,
+        );
+      }
+
+      console.log(error);
+      
+      throw new InternalServerErrorException(
+        'Error at the moment to create the Pokemon, for more information, see on the logs',
+      );
+    }
   }
 
   remove(id: number) {
