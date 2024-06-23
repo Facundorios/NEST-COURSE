3092cab9add1eb67555929f3dffda42e8e3dc335
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-22 21:46:59 -0300

    feat: Post: Receive and validate data

--- 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsNumber, IsPositive, IsString, Min, MinLength } from "class-validator"
+
+export class CreatePokemonDto {
+    
+    @Min(1)  
+    @IsInt()
+    @IsPositive()
+    no: number
+
+    @MinLength(3)
+    @IsString()
+    name:string
+}
