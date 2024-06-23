35590b0ef71c1508ecf65dc1946f2b5e79577edd
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-22 21:56:40 -0300

    feat: added insert pokemon on db

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
 
-    createPokemonDto.name = createPokemonDto.name.toLowerCase()
+    const pokemon = this.pokemonModel.create(createPokemonDto)
 
-    return createPokemonDto;
+    return pokemon;
   }
 
   findAll() {
