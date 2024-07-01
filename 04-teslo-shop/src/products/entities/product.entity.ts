61a1600e413ad5a96db55a1ce1e2ee78d6e07a9b
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-30 22:17:32 -0300

    feat: added entities without relations

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -1,14 +1,48 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
 
-    @Column('text', {
-        unique: true
-    })
-    title: string
+  @Column('numeric', {
+    default: 0,
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    unique: true,
+    nullable: false,
+  })
+  slug: string;
+
+  @Column({
+    type: 'int',
+    default: 0,
+  })
+  stock: number;
+
+  @Column({
+    type: 'text',
+    array: true,
+    nullable: true,
+  })
+  sizes: string[];
+
+  @Column({
+    type: 'text'
+  })
+  gender: string
 
 }
