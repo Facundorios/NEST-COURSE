9b33ab231ee7a5205da3ab38f3c9813fab07fb51
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-30 23:09:30 -0300

    fix: handleDatabasEexceptions

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -1,13 +1,27 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 
+
 @Injectable()
 export class ProductsService {
   
+
+  private handleDatabaseExceptions(error: any) {
+    //Manejos de errores.
+      //Si el error es de tipo 23505, significa que se violó una restricción de unicidad en la base de datos, en este caso, la restricción de unicidad del campo slug
+      if (error.code === '23505') {
+        throw new InternalServerErrorException(error.detail)
+      }
+      // this.logger.error(error.message)
+      //Si el error no es de tipo 23505, lanzamos un error 500
+      throw new InternalServerErrorException("Unexpected error, check logs")
+  }
+
+  private readonly logger = new Logger('ProdctsService')
   constructor(
     //Inyectamos el repositorio de Product. Un repositorio es un objeto que se encarga de realizar operaciones de base de datos, como insertar, actualizar, eliminar y consultar registros.
     @InjectRepository(Product)
@@ -27,8 +41,7 @@ export class ProductsService {
       return newProduct
 
     } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException("HOLA")
+      this.handleDatabaseExceptions(error)
     }
 
 
