c606f792a3a9386b46879c71024fae331914c731
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-30 22:30:21 -0300

    feat: create product DTO

--- 04-teslo-shop/src/products/dto/create-product.dto.ts
+++ 04-teslo-shop/src/products/dto/create-product.dto.ts
@@ -1,1 +1,42 @@
-export class CreateProductDto {}
+import {
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(5)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price: number;
+
+  @IsString()
+  @MinLength(15)
+  description?: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  //Con la propiedad de IsString() "each", puedo hacer que cada uno de los elementos del array sean de tipo string
+  @IsString({ each: true })
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'other'])
+  @IsString()
+  gender: string;
+}
