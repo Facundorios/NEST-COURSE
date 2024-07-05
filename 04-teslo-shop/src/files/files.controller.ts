d9163e8b6536c9028334c8dd3ce24f6745760237
Facundo Ríos <Facundorios2005@gmail.com>
2024-07-05 01:20:07 -0300

    feat: return image client

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,7 +1,10 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -9,26 +12,44 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileNamer, fileFilter } from './helpers';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findeOneImage(
+    //El decorador Res sirve para devolver un archivo estático, en el momento en que lo utilizamos, la aplicación no va a devolver nada y va a esperar una respuesta.
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    //Se obtiene el path de la imagen
+    const path = this.filesService.getProductImage(imageName);
+
+    //Se envia el archivo al cliente
+    // res.status(403).json({
+    //   ok: false,
+    //   path: path,
+    // });
+
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileFilter,
       storage: diskStorage({
-        destination: './static/uploads',
-        filename: fileNamer
-      })
+        destination: './static/products',
+        filename: fileNamer,
+      }),
     }),
   )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) throw new BadRequestException('Archivo vacío / no compartible');
 
-    return {
-      fileName: file.originalname,
-    };
+    const secureURL = `${file.filename}`;
+    return { secureURL };
   }
 }
--- 04-teslo-shop/src/files/files.service.ts
+++ 04-teslo-shop/src/files/files.service.ts
@@ -1,7 +1,20 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
+  //Se obtiene la imagen del producto
+  getProductImage(imageName: string) {
+    //Se crea el path fisico de donde se encuentra la imagen en el servidor.
+    const path = join(__dirname, '../../static/products', imageName);
+    //Se verifica si la imagen existe, si no existe se lanza una excepción. el existsSync es una función que verifica si un archivo existe en el path que se le pasa como argumento.
+    if (!existsSync(path))
+      throw new BadRequestException(
+        `Not product found with imagen ${imageName}`,
+      );
 
+    //Se retorna el path de la imagen
+    return path;
+  }
 }
