56d228b5bb0a2afedf9fe73a8807606c7884bb68
Facundo Ríos <Facundorios2005@gmail.com>
2024-07-09 17:58:53 -0300

    feat: JwtStrategy #2

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
@@ -7,12 +8,15 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 import { AuthController } from './auth.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt-strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+
+    ConfigModule,
+
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
 
@@ -36,5 +40,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       },
     }),
   ],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule]
 })
 export class AuthModule {}
--- 04-teslo-shop/src/auth/strategies/jwt-strategy.ts
+++ 04-teslo-shop/src/auth/strategies/jwt-strategy.ts
@@ -1,14 +1,46 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { User } from '../entities/user.entity';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
+
+@Injectable()
 //Se crea la clase JwtStrategy que extiende de PassportStrategy, una función que recibe como parámetro la estrategia que se va a utilizar, en este caso Strategy de passport-jwt.
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  async validate(payload: JwtPayload): Promise<User> {
+  //Se inyecta el repositorio de usuarios y el servicio de configuración.
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    configService: ConfigService,
+  ) {
+    //Se llama al constructor de la clase padre con un objeto que contiene la clave secreta y el token JWT.
+    //Porque necesito poner Super? = Porque estoy extendiendo de PassportStrategy y necesito llamar al constructor de la clase padre.
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
 
-    const { email } = payload
+  //Se crea el método validate que recibe un payload de tipo JwtPayload y retorna un usuario
+  async validateMethod(payload: JwtPayload): Promise<User> {
+    //Se extrae el email del payload
+    const { email } = payload;
 
-    return;
+    //Se busca el usuario en la base de datos por el email
+    const user = this.userRepository.findOneBy({ email: email });
+    //Se valida si el usuario existe
+    if (!user) {
+      throw new UnauthorizedException(`Token no valid`);
+    }
+    //Se valida si el usuario está activo
+    if (!(await user).isActive) {
+      throw new UnauthorizedException(`User is not active`);
+    }
+
+    return user;
   }
 }
